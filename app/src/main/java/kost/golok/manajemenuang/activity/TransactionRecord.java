9183d58dfc95edb118d47fc6040bbbd2eb74b042
GoLok56 <satadii11@gmail.com>
2017-04-03 03:19:29 +0700

    double backpressed to exit

--- app/src/main/java/kost/golok/manajemenuang/activity/TransactionRecord.java
+++ app/src/main/java/kost/golok/manajemenuang/activity/TransactionRecord.java
@@ -3,28 +3,35 @@ package kost.golok.manajemenuang.activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Date;
+
 import kost.golok.adapter.PageAdapter;
 import kost.golok.manajemenuang.R;
+import kost.golok.utility.Formatter;
 import kost.golok.utility.Preference;
 
 public class TransactionRecord extends AppCompatActivity {
+
+    public static final int TIME_DELAY = 5000;
+    public static long back_pressed;
 
     @SuppressWarnings("ConstantConditions")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Get rid the shadow below ActionBar
         getSupportActionBar().setElevation(0);
 
@@ -46,6 +53,17 @@ public class TransactionRecord extends AppCompatActivity {
         // Setting up the tab
         TabLayout tab = (TabLayout) findViewById(R.id.tab);
         tab.setupWithViewPager(vp);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (back_pressed + TIME_DELAY > System.currentTimeMillis()) {
+            finish();
+            super.onBackPressed();
+        } else
+            Toast.makeText(TransactionRecord.this, "Press back again to exit!", Toast.LENGTH_SHORT).show();
+
+        back_pressed = System.currentTimeMillis();
     }
 
     private Dialog getDialog(final SharedPreferences pref) {
