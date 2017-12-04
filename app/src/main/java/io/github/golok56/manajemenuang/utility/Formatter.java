13d0e7a40643b39e12d0e80d4a9cb819e00594c2
Satria Adi Putra <satadii11@gmail.com>
2017-12-04 13:19:21 +0700

    change formatDate algorithm to accept String argument

--- app/src/main/java/io/github/golok56/manajemenuang/utility/Formatter.java
+++ app/src/main/java/io/github/golok56/manajemenuang/utility/Formatter.java
@@ -1,4 +1,4 @@
-package io.github.golok56.utility;
+package io.github.golok56.manajemenuang.utility;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
@@ -6,14 +6,12 @@ import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import io.github.golok56.models.Transaction;
-
 public class Formatter {
 
     private Formatter() {
     }
 
-    public static String formatCurrency(Double value) {
+    public static String formatCurrency(double value) {
         NumberFormat nf = NumberFormat.getCurrencyInstance();
         DecimalFormatSymbols format = new DecimalFormatSymbols();
         format.setCurrencySymbol("Rp ");
@@ -24,13 +22,9 @@ public class Formatter {
         return df.format(value);
     }
 
-    public static String formatDate(Date date) {
-        SimpleDateFormat format = new SimpleDateFormat("dd MMMM yyyy");
-        return format.format(date);
-    }
-
-    public static String formatAmount(Transaction transaksi){
-        return transaksi.getAmount().replace("Rp ", "").replace(".", "").replace(",00", "");
+    public static String formatDate(String dateFormat) {
+        SimpleDateFormat format = new SimpleDateFormat(dateFormat);
+        return format.format(new Date());
     }
 
 }
