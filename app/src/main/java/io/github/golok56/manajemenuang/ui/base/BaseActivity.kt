4f74a4964ba5da1efed032d4994647277f35f16f
Satria Adi Putra <satadii11@gmail.com>
2017-12-19 13:21:22 +0700

    add start function to navigate between activity

--- app/src/main/java/io/github/golok56/manajemenuang/ui/base/BaseActivity.kt
+++ app/src/main/java/io/github/golok56/manajemenuang/ui/base/BaseActivity.kt
@@ -1,15 +1,29 @@
 package io.github.golok56.manajemenuang.ui.base
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 
 /**
  * @author Satria Adi Putra
  */
+@SuppressLint("Registered")
 open class BaseActivity : AppCompatActivity() {
 
     fun showToast(msg: String) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    fun start(activityClass: Class<*>){
+        startActivity(Intent(this, activityClass))
+        finish()
+    }
+
+    fun start(activityClass: Class<*>, extraName: String, extra: Long){
+        val intent = Intent(this, activityClass)
+        intent.putExtra(extraName, extra)
+        startActivity(intent)
     }
 
 }
