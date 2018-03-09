892ece97b6a5615d57a5e3b9cf9080cb6671cf00
Satria Adi Putra <satadii11@gmail.com>
2018-03-09 13:33:55 +0700

    update database entity

--- app/src/main/java/io/github/golok56/manajemenuang/models/Transaction.java
+++ app/src/main/java/io/github/golok56/manajemenuang/models/Transaction.java
@@ -1,6 +1,8 @@
 package io.github.golok56.manajemenuang.models;
 
+import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
 import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
 
@@ -8,7 +10,11 @@ import android.arch.persistence.room.PrimaryKey;
  * This class store the information about transaction such as
  * amount, date, type, and description of transaction
  */
-@Entity
+@Entity(foreignKeys = @ForeignKey(
+        entity = Category.class,
+        parentColumns = "id",
+        childColumns = "category_id"
+))
 public class Transaction {
 
     public static final int EXPENSE_TYPE = 0;
@@ -16,45 +22,52 @@ public class Transaction {
 
     @PrimaryKey(autoGenerate = true)
     private long id;
+
     private double amount;
     private int type;
     private String date;
+    private String month;
+    private String year;
     private String description;
 
-    /**
-     * Create an instance of {@link Transaction}
-     *
-     * @param amount      the total amount of transaction
-     * @param description the description of the transaction
-     * @param date        the date of transaction
-     * @param type        the type of transaction
-     */
-    public Transaction(long id, double amount, String description, String date, int type) {
+    @ColumnInfo(name = "category_id")
+    private long categoryId;
+
+    public Transaction() {
+    }
+
+    public Transaction(long id, double amount, String description, String date, String month,
+                       String year, int type, long categoryId) {
         this.id = id;
         this.amount = amount;
         this.description = description;
         this.date = date;
+        this.month = month;
+        this.year = year;
         this.type = type;
+        this.categoryId = categoryId;
     }
 
     @Ignore
-    public Transaction(double amount, String description, String date, int type) {
+    public Transaction(double amount, String description, String date, String month,
+                       String year, int type, long categoryId) {
         this.amount = amount;
         this.description = description;
         this.date = date;
+        this.month = month;
+        this.year = year;
         this.type = type;
+        this.categoryId = categoryId;
     }
 
-    /**
-     * Return if of transaction
-     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public long getId() {
         return id;
     }
 
-    /**
-     * Return the total amount of transaction
-     */
     public double getAmount() {
         return amount;
     }
@@ -63,30 +76,25 @@ public class Transaction {
         this.amount = amount;
     }
 
-    /**
-     * Return the type of transaction
-     */
     public int getType() {
         return type;
+    }
+
+    public String getStringType() {
+        switch (type) {
+            case EXPENSE_TYPE:
+                return "Pengeluaran";
+            case INCOME_TYPE:
+                return "Pemasukan";
+            default:
+                return "";
+        }
     }
 
     public void setType(int type) {
         this.type = type;
     }
 
-    public String getTypeText() {
-        switch (type) {
-            case EXPENSE_TYPE:
-                return "Pengeluaran";
-            case INCOME_TYPE:
-                return "Pemasukan";
-        }
-        return "";
-    }
-
-    /**
-     * Return the description of transaction
-     */
     public String getDescription() {
         return description;
     }
@@ -101,5 +109,37 @@ public class Transaction {
 
     public void setDate(String date) {
         this.date = date;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getFullDate() {
+        return date + " " + month + " " + year;
+    }
+
+    public boolean isIncome() {
+        return type == INCOME_TYPE;
     }
 }
